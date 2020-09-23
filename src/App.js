2e60d124d2ed952f5ddc502379712fd32302b10b
Michael Siu <michaeltsiu13@gmail.com>
2020-09-23 14:10:34 -0700

    Home/login page works as well as spotify tokens

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from 'react';
+import './styling/App.css';
+import Login from './Login';
+import { getTokenFromUrl } from './spotify'
+import SpotifyWebApi from "spotify-web-api-js";
+
+const spotify = new SpotifyWebApi();
+
+function App() {
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+    if (_token) {
+      setToken(_token)
+      spotify.setAccessToken(_token);
+      spotify.getMe().then(user => {
+        console.log('user: ', user);
+      })
+    }
+    console.log('I HAVE A TOKEN: ', token);
+  }, []);
+
+  return (
+    <div className="app">
+      {
+        token ? (
+          <h1>I am logged in</h1>
+          // <Player />
+        ) : (
+          <Login />
+        )
+      }
+    </div>
+  );
+}
+
+export default App;
